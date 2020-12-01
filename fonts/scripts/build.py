fa0102a5bf319b96456f01d2373910f31fe303b0
Eigo <e.k.net@outlook.com>
2020-12-01 22:56:15 +0100

    Replaced NODE_ENV with VERCEL_ENV

--- fonts/scripts/build.py
+++ fonts/scripts/build.py
@@ -2,7 +2,7 @@ import os
 import sys
 import subprocess
 
-if 'NODE_ENV' in os.environ and os.environ['NODE_ENV'] == 'production':
+if 'VERCEL_ENV' in os.environ and os.environ['VERCEL_ENV'] == 'production':
   subprocess.check_call('python3', 'build-fonts.py')
 else:
   subprocess.check_call(['pipenv', 'run', 'python3', 'build-fonts.py'])
--- fonts/scripts/install.py
+++ fonts/scripts/install.py
@@ -2,7 +2,7 @@ import os
 import sys
 import subprocess
 
-if 'NODE_ENV' in os.environ and os.environ['NODE_ENV'] == 'production':
+if 'VERCEL_ENV' in os.environ and os.environ['VERCEL_ENV'] == 'production':
   subprocess.check_call(['pip3', 'install'])
 else:
   subprocess.check_call(['pipenv', 'install'])
