49305a21f5ac1245e4caf571940da3eba4070b4d
Eigo <e.k.net@outlook.com>
2020-12-01 23:47:04 +0100

    Switched to sh

--- fonts/scripts/preinstall.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-import subprocess
-
-if 'VERCEL_ENV' in os.environ and os.environ['VERCEL_ENV'] == 'production':
-  subprocess.check_call([sys.executable, '-m', 'pip3', 'install', '-Iv', 'pipenv==2020.11.15'])
---
+++ fonts/scripts/preinstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+if [ "$VERCEL_ENV" = "production" ]
+then
+  pip3 install -Iv pipenv==2020.11.15
+fi
