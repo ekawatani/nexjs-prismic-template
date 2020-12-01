88f8a71fd01e83a2edc48e8f8f5c4cfa60925b44
Eigo <e.k.net@outlook.com>
2020-12-01 23:19:55 +0100

    Install pipenv in preinstall task

--- fonts/scripts/preinstall.py
+++ fonts/scripts/preinstall.py
@@ -4,5 +4,3 @@ import subprocess
 
 if 'VERCEL_ENV' in os.environ and os.environ['VERCEL_ENV'] == 'production':
   subprocess.check_call(['pip3' 'install', '-Iv', 'pipenv==2020.11.15'])
-
-subprocess.check_call(['pipenv', 'install'])
