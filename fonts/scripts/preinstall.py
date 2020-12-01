cbd83f5c2a925b24bbb4c8205bf5cc7bd352b2a3
Eigo <e.k.net@outlook.com>
2020-12-01 23:29:46 +0100

    Updated preinstall script

--- fonts/scripts/preinstall.py
+++ fonts/scripts/preinstall.py
@@ -3,4 +3,4 @@ import sys
 import subprocess
 
 if 'VERCEL_ENV' in os.environ and os.environ['VERCEL_ENV'] == 'production':
-  subprocess.check_call(['pip3' 'install', '-Iv', 'pipenv==2020.11.15'])
+  subprocess.check_call([sys.executable, '-m', 'pip3', 'install', '-Iv', 'pipenv==2020.11.15'])
